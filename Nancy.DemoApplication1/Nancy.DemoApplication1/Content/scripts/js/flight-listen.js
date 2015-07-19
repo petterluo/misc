8c08b4429750b0106fbe2dabc71d9590a7c96bec
petterluo <petter@di.se>
2015-07-19 14:28:07 +0200

    changes to flight files

--- Nancy.DemoApplication1/Nancy.DemoApplication1/Content/scripts/js/flight-listen.js
+++ Nancy.DemoApplication1/Nancy.DemoApplication1/Content/scripts/js/flight-listen.js
@@ -2,20 +2,34 @@
 define(['../vendor/flight/index'], function (flight) {
     'use strict';
 
+
+    //this.on(document, 'dataSent', function (ev, data) {
+    //    alert('Message sent: ' + data.msg);
+    //});
+
+
     var listen = flight.component(function () {
 
-        this.attributes({
-            text: null
-        });
+        //this.attributes({
+        //    text: null
+        //});
 
-        this.doListening = function () {
-            console.log('listening');
-            this.$node.html(this.attr.text);
-        }
+        //this.doListening = function () {
+        ////this.doListening = function (ev, data) {
+        //    console.log('listening');
+        //    this.$node.html(this.attr.text);
+        //}
 
         //initialize
-        this.after('initialize', function () {
-            this.on(document, 'talk', this.doListening);
+        //this.after('initialize', function () {
+        this.after('initialize', function (ev, data) {
+            //this.on(document, 'talk', this.doListening);
+            //var data = "abc";
+
+            this.on(document, 'talk', function () { console.log('listening to ' + data) });
+            
+
+            //this.on(document, 'talk', this.doListening);
         });
 
     });
--- Nancy.DemoApplication1/Nancy.DemoApplication1/Content/scripts/js/flight-talk.js
+++ Nancy.DemoApplication1/Nancy.DemoApplication1/Content/scripts/js/flight-talk.js
@@ -2,16 +2,29 @@
 define(['../vendor/flight/index'], function (flight) {
     'use strict';
 
+    //this.on(document, 'dataSent', function (ev, data) {
+    //    alert('Message sent: ' + data.msg);
+    //});
+
     var talk = flight.component(function () {
 
-        this.doTalking = function (event, data) {
-            console.log('talking');
-            this.trigger(document, 'talk');
-        };
+        ////this.doTalking = function (ev, data) {
+        //this.doTalking = function () {
+        //    console.log('talking');
+        //    //data = 'test123';
+        //    this.trigger(document, 'talk');
+        //};
 
         //initialize
         this.after('initialize', function () {
-            this.on('click', this.doTalking);
+            //this.on('click', this.doTalking);
+
+            this.on('click', function () {
+                //var ev = 'the event';
+                //var data = 'the data';
+                this.trigger(document, 'talk', { data: 'some data' });
+
+            });
         });
     });
 
