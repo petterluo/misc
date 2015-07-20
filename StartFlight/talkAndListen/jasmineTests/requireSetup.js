d6339f8c1057662e76471b533a930c73c23a1ade
Petter Luotsinen <petter@di.se>
2015-07-20 16:15:57 +0200

    some updates to StartFlight

---
+++ StartFlight/talkAndListen/jasmineTests/requireSetup.js
@@ -0,0 +1,24 @@
+requirejs.config({
+    baseUrl: '',
+    paths: {
+        'flight': '../../vendor/flight'
+  }
+});
+
+//require(
+//    ['spec/tmpSpec'],
+//    function (init) {
+//        init();
+//    }
+//);
+
+
+//requirejs.config({
+//    baseUrl: '../../vendor/flight',
+//    paths: {
+//        'flight': '../../vendor/flight',
+//        'component': '../../vendor/flight/lib/component'
+//  }
+//});
+
+
---
+++ StartFlight/talkAndListen/jasmineTests/spec/tmpSpec.js
@@ -0,0 +1,19 @@
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+      expect(true).toBe(true);
+      //expect(true).toBe(false);
+  });
+
+});
+
+//describe("A suite", function () {
+
+    //describeComponent('ui/text_input', function () {
+    //    it('gets the value of the input field it is attached to', function () {
+    //        this.setupComponent('<input type="text" value="hello world" />');
+    //        expect(this.component.getValue()).toEqual('hello, world');
+    //        //expect(true).toBe(true);
+    //    });
+    //});
+
+//});
